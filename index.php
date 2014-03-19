bc99d8324f5321748a1b23b230f47a1e9adf5eef
vlad <vlzakar@gmail.com>
2014-03-19 15:12:27 +0100

    Improved app structure
    Removed unnecessary files from extjs and added only needed.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="extjs/resources/css/ext-all.css">
 	<link rel="stylesheet" type="text/css" href="resources/css/extjs-custom.css">
     <script type="text/javascript" src="extjs/ext-all.js"></script>
-    <script type="text/javascript" src="app.js"></script>
+    <script type="text/javascript" src="app-all.js"></script>
 </head>
 <body>
 </body>
